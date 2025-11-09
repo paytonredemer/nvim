7905b4d9446018bdca14e96441fb8d451f2174aa
Payton Redemer <paytonredemer@gmail.com>
2025-11-09 14:26:44 -0600

    feat(codecompanion): add keymaps for chat and actions

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,4 +6,9 @@ return {
   },
   cmd = { "CodeCompanion", "CodeCompanionActions", "CodeCompanionChat", "CodeCompanionCmd" },
   opts = {},
+  keys = {
+    { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Code Companion Toggle" },
+    { "<leader>an", "<cmd>CodeCompanionChat<cr>",        desc = "Code Companion New Chat" },
+    { "<leader>am", "<cmd>CodeCompanionActions<cr>",     desc = "Code Companion More" },
+  }
 }
