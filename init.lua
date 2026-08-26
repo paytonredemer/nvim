fb69ec904fa7bbf9464adc7bbc3067b1f34d0631
Payton Redemer <dev@paytonr.com>
2026-08-26 15:54:23 -0500

    refactor: configure Lazy through lazyCat

--- init.lua
+++ init.lua
@@ -4,20 +4,25 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 
-require("lazy").setup("plugins", {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        "netrwPlugin",
-        "rplugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+require("nixCatsUtils.lazyCat").setup(
+  nixCats.pawsible({ "allPlugins", "start", "lazy.nvim" }),
+  "plugins",
+  {
+    lockfile = nixCats.configDir .. "/lazy-lock.json",
+    performance = {
+      rtp = {
+        disabled_plugins = {
+          "gzip",
+          -- "matchit",
+          -- "matchparen",
+          "netrwPlugin",
+          "rplugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
       },
     },
-  },
-})
+  }
+)
