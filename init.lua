14c1477796df1fb9ee8f05a1263501cd93388fb5
Payton Redemer <dev@paytonr.com>
2026-08-26 16:01:13 -0500

    fix: use the wrapped config path for Lazy

--- init.lua
+++ init.lua
@@ -4,7 +4,10 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 
+local config_dir = vim.g.packaged_config_dir or vim.fn.stdpath("config")
+
 require("lazy").setup("plugins", {
+  lockfile = config_dir .. "/lazy-lock.json",
   performance = {
     rtp = {
       disabled_plugins = {
